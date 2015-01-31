af1a5f2b9171d889f7781bf204826f65e84c57ad
FenrisWulf <fenriswoolf@gmail.com>
2015-01-31 14:58:54 -0600

    login stuff

---
+++ bknd/main.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var server = http.createServer();
+var fs = require('fs');
+
+server.on('request', function (request, res, next) {
+  var body = "";
+        // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  request.on('data', function (chunk) {
+    body += chunk;
+  });
+  request.on('end', function() {
+   console.log(body);
+   var data = JSON.parse(body);
+   
+    res.writeHead(200);
+   if (data.type=="login") {
+
+            res.write(JSON.stringify(login(data)));
+
+   }
+    
+            res.end();
+  });
+});
+
+
+server.listen(3000);
+
+function login(data) {
+    console.log("logging in");
+    return {data:"true"}
+}
